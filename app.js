3867b656759ebe5dc8752082c670e1504c49bfd5
kavyansh <kavyansh21sharma@gmail.com>
2022-10-27 13:31:42 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const endDate = "21 December 2022 00:00 pm "
+
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input")
+
+function clock() {
+    const end = new Date(endDate);
+    const now = new Date()
+    const diff = (end - now) / 1000 ;
+
+    if (diff<0) return;
+    
+    //  convert in days
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    // convert into hours 
+    inputs[1].value = (Math.floor(diff/3600)%24);
+    // convert into minutes 
+    inputs[2].value = (Math.floor(diff/60)%60);
+    // convert into seconds 
+    inputs[3].value= Math.floor(diff) % 60;
+  
+}
+// intial call 
+clock()
+
+
+//  function to reload page every second 
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
+
+
+
+ 
+
+
+
